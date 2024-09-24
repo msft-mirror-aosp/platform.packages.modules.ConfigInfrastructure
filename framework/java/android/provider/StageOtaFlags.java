5211ede1a31841ea46da5b547cd00e4d152c9113
Liana Kazanova (xWF) <lkazanova@google.com>
2024-09-24 23:14:51 +0000

    Revert "Implement stageFlagsForBuild API"
    Revert submission 29565776-configinfra-stageapi
    
    Reason for revert: DroidMonitor: Potential culprit for b/369442252 - verifying through ABTD before revert submission. This is part of the standard investigation process, and does not mean your CL will be reverted.
    
    Reverted changes: /q/submissionid:29565776-configinfra-stageapi
    
    Change-Id: Ia9108ee065f56546b4e62e26cfd6f6542c1613ac

--- framework/java/android/provider/StageOtaFlags.java
+++ framework/java/android/provider/StageOtaFlags.java
@@ -13,25 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package android.provider;
 
-import android.aconfigd.Aconfigd.FlagOverride;
-import android.aconfigd.Aconfigd.StorageRequestMessage;
-import android.aconfigd.Aconfigd.StorageRequestMessages;
-import android.aconfigd.Aconfigd.StorageReturnMessages;
 import android.annotation.FlaggedApi;
 import android.annotation.NonNull;
 import android.annotation.SystemApi;
-import android.net.LocalSocket;
-import android.net.LocalSocketAddress;
 import android.provider.flags.Flags;
-import android.util.AndroidRuntimeException;
 import android.util.Log;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.util.Map;
 
 /** @hide */
@@ -39,24 +28,27 @@ import java.util.Map;
 @FlaggedApi(Flags.FLAG_STAGE_FLAGS_FOR_BUILD)
 public final class StageOtaFlags {
   private static String LOG_TAG = "StageOtaFlags";
-  private static final String SOCKET_ADDRESS = "aconfigd";
 
   private StageOtaFlags() {}
 
   /**
    * Stage aconfig flags to be applied when booting into {@code buildId}.
    *
-   * <p>Only a single {@code buildId} and its corresponding flags are stored at once. Every
-   * invocation of this method will overwrite whatever mapping was previously stored.
+   * <p>Only a single {@code buildId} and its corresponding flags are stored at
+   * once. Every invocation of this method will overwrite whatever mapping was
+   * previously stored.
    *
-   * <p>It is an implementation error to call this if the storage is not initialized and ready to
-   * receive writes. Callers must ensure that it is available before invoking.
+   * It is an implementation error to call this if the storage is not
+   * initialized and ready to receive writes. Callers must ensure that it is
+   * available before invoking.
    *
-   * <p>TODO(b/361783454): create an isStorageAvailable API and mention it in this docstring.
+   * TODO(b/361783454): create an isStorageAvailable API and mention it in this
+   * docstring.
    *
    * @param flags a map from {@code <packagename>.<flagname>} to flag values
    * @param buildId when the device boots into buildId, it will apply {@code flags}
-   * @throws AndroidRuntimeException if communication with aconfigd fails
+   * @throws IllegalStateException if the storage is not ready to receive writes
+   *
    * @hide
    */
   @SystemApi
@@ -65,74 +57,5 @@ public final class StageOtaFlags {
       @NonNull Map<String, Boolean> flags, @NonNull String buildId) {
     int flagCount = flags.size();
     Log.d(LOG_TAG, "stageFlagsForBuild invoked for " + flagCount + " flags");
-
-    try {
-      LocalSocket socket = new LocalSocket();
-      LocalSocketAddress address =
-          new LocalSocketAddress(SOCKET_ADDRESS, LocalSocketAddress.Namespace.RESERVED);
-      if (!socket.isConnected()) {
-        socket.connect(address);
-      }
-      InputStream inputStream = socket.getInputStream();
-      OutputStream outputStream = socket.getOutputStream();
-
-      StorageRequestMessages requestMessages = buildRequestMessages(flags, buildId);
-
-      writeToSocket(outputStream, requestMessages);
-      readFromSocket(inputStream);
-    } catch (IOException e) {
-      throw new AndroidRuntimeException(e);
-    }
-  }
-
-  private static void writeToSocket(
-      OutputStream outputStream, StorageRequestMessages requestMessages) throws IOException {
-    byte[] messageBytes = requestMessages.toByteArray();
-    outputStream.write(ByteBuffer.allocate(4).putInt(messageBytes.length).array());
-    outputStream.write(messageBytes);
-    outputStream.flush();
-  }
-
-  private static StorageReturnMessages readFromSocket(InputStream inputStream) throws IOException {
-    byte[] lengthBytes = new byte[4];
-    int bytesRead = inputStream.read(lengthBytes);
-    if (bytesRead != 4) {
-      throw new IOException("Failed to read message length");
-    }
-
-    int messageLength = ByteBuffer.wrap(lengthBytes).order(ByteOrder.BIG_ENDIAN).getInt();
-
-    byte[] messageBytes = new byte[messageLength];
-    bytesRead = inputStream.read(messageBytes);
-    if (bytesRead != messageLength) {
-      throw new IOException("Failed to read complete message");
-    }
-
-    return StorageReturnMessages.parseFrom(messageBytes);
-  }
-
-  private static StorageRequestMessages buildRequestMessages(
-      @NonNull Map<String, Boolean> flags, @NonNull String buildId) {
-    StorageRequestMessage.OTAFlagStagingMessage.Builder otaMessageBuilder =
-        StorageRequestMessage.OTAFlagStagingMessage.newBuilder().setBuildId(buildId);
-    for (Map.Entry<String, Boolean> flagAndValue : flags.entrySet()) {
-      String qualifiedFlagName = flagAndValue.getKey();
-      String packageName = qualifiedFlagName.substring(0, qualifiedFlagName.lastIndexOf("."));
-      String flagName = qualifiedFlagName.substring(qualifiedFlagName.lastIndexOf(".") + 1);
-      String value = flagAndValue.getValue() ? "true" : "false";
-      FlagOverride override =
-          FlagOverride.newBuilder()
-              .setPackageName(packageName)
-              .setFlagName(flagName)
-              .setFlagValue(value)
-              .build();
-      otaMessageBuilder.addOverrides(override);
-    }
-    StorageRequestMessage.OTAFlagStagingMessage otaMessage = otaMessageBuilder.build();
-    StorageRequestMessage requestMessage =
-        StorageRequestMessage.newBuilder().setOtaStagingMessage(otaMessage).build();
-    StorageRequestMessages requestMessages =
-        StorageRequestMessages.newBuilder().addMsgs(requestMessage).build();
-    return requestMessages;
   }
 }
